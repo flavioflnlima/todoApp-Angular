e718b3d8b72daa0847664bf9a67e1e4fbfe9e52f
Flavio Lima do nascimento <flavioflnlima@gmail.com>
2020-07-07 22:42:55 -0300

    iniciado a config do app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //<app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'minhas tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'cortar o cabelo', false));
+    this.todos.push(new Todo(3, 'ir ao mercado', true));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+  }
+
+  markAsDone() {
+
+  }
+
+  markAsUnDone() {
+
+  }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,3 @@
+export class Todo {
+  constructor(public id: Number, public description: String, public done: Boolean) {}
+}
